e0563a2ac719f9365332820f7bdc8642dde5a15c
Tamas Nepusz <ntamas@gmail.com>
2023-01-18 17:47:06 +0100

    feat: added EdgeSequenceType and VertexSequenceType enum

--- src/igraph_ctypes/_internal/enums.py
+++ src/igraph_ctypes/_internal/enums.py
@@ -1,6 +1,39 @@
 from enum import IntEnum
 
-__all__ = ("NeighborMode", "StarMode", "TreeMode", "WheelMode")
+__all__ = (
+    "EdgeSequenceType",
+    "MatrixStorage",
+    "NeighborMode",
+    "StarMode",
+    "TreeMode",
+    "VertexSequenceType",
+    "WheelMode",
+)
+
+
+class EdgeSequenceType(IntEnum):
+    """Python counterpart of an ``igraph_es_type_t`` enum."""
+
+    ALL = 0
+    ALLFROM = 1
+    ALLTO = 2
+    INCIDENT = 3
+    NONE = 4
+    ONE = 5
+    VECTORPTR = 6
+    VECTOR = 7
+    RANGE = 8
+    PAIRS = 9
+    PATH = 10
+    UNUSED_WAS_MULTIPAIRS = 11
+    ALL_BETWEEN = 12
+
+
+class MatrixStorage(IntEnum):
+    """Python counterpart of an ``igraph_matrix_storage_t`` enum."""
+
+    ROW_MAJOR = 0
+    COLUMN_MAJOR = 1
 
 
 class NeighborMode(IntEnum):
@@ -26,6 +59,19 @@ class TreeMode(IntEnum):
     OUT = 0
     IN = 1
     UNDIRECTED = 2
+
+
+class VertexSequenceType(IntEnum):
+    """Python counterpart of an ``igraph_vs_type_t`` enum."""
+
+    ALL = 0
+    ADJ = 1
+    NONE = 2
+    ONE = 3
+    VECTORPTR = 4
+    VECTOR = 5
+    RANGE = 6
+    NONADJ = 7
 
 
 class WheelMode(IntEnum):
